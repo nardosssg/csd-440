3f1940073e2216ff5df5042597652135b081b153
Nardos G <you@email.com>
2026-01-18 02:09:18 -0600

    pushed module 10 assignment

---
+++ NardosFirstProgram.php
@@ -0,0 +1,16 @@
+<?php
+/****************************************************
+ * Nardos Gebremedhin
+ * Module 1: XAMPP & PHP First Program
+ * 1/18/2026
+
+ * This program displays a welcome message using PHP
+ * to verify that XAMPP and PHP are installed and
+ * functioning correctly.
+ ****************************************************/
+
+// Display a welcome message
+echo "<h1>PHP is working correctly!</h1>";
+echo "<p>This is my first PHP program using XAMPP.</p>";
+
+?>
